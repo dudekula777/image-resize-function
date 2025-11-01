9ec4a04a18aef06778e4c385cb675e355310c94c
dudekula777 <dudekula777@gmail.com>
2025-11-01 16:46:15 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-  use_cli = true
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 
