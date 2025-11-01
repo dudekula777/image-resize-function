dfbe3d020f4c33d915f44a15261603c77d5f45d1
dudekula777 <dudekula777@gmail.com>
2025-11-01 16:24:37 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_service_plan" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "Y1"
 }
 
 # 🔹 Function App
