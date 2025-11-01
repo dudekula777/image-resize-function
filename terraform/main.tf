fe69733b00ec7e02069377ae73817220353f96bb
dudekula777 <dudekula777@gmail.com>
2025-11-01 16:49:36 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,16 +10,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
+
+  features {}
 }
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -47,3 +47,24 @@ variable "service_plan_sku" {
   type        = string
   default     = "F1"
 }
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
