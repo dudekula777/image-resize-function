1b4edcf061ee68d76fb6f2b07d10784cfa6311e9
dudekula777 <dudekula777@gmail.com>
2025-11-01 16:10:56 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_service_plan" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   os_type             = "Linux"
-  sku_name            = "Y1" # ✅ Free serverless plan
+  sku_name            = "B1"
 }
 
 # 🔹 Function App
