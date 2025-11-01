54849e2112373937916fb33d2ab9f6a604b975cf
dudekula777 <dudekula777@gmail.com>
2025-11-01 18:33:24 +0530

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,22 +49,46 @@ variable "service_plan_sku" {
 }
 
 variable "subscription_id" {
-  description = "Azure Subscription ID"
+  description = "Azure subscription ID"
   type        = string
+  default     = ""
 }
 
 variable "tenant_id" {
-  description = "Azure Tenant ID"
+  description = "Azure tenant ID"
   type        = string
+  default     = ""
 }
 
 variable "client_id" {
-  description = "Azure Service Principal Client ID"
+  description = "Azure client ID"
   type        = string
+  default     = ""
 }
 
 variable "client_secret" {
-  description = "Azure Service Principal Client Secret"
+  description = "Azure client secret"
   type        = string
-  sensitive   = true
+  default     = ""
+}
+
+variable "application_insights_name" {
+  description = "Name of the Application Insights resource"
+  type        = string
+  default     = "app-insights-image-resize"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "plan-imgresize"
+}
+variable "application_insights_name" {
+  description = "Name of the Application Insights instance"
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
 }
